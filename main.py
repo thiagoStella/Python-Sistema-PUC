5460132ab84077f8584234843d590397ae9444e0
thiagoStella <thiagostellapontes01@gmail.com>
2024-03-19 17:36:09 -0300

    alteracao de if-else para match-case

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+print("                                        ")
 print("****************************************")
 print("*****  Bem-Vindo ao Sistema PUC  *******")
+print("*****  ------------------------  *******")
 print("****************************************")
+print("                                        ")
 
 # mostrar menu
 print("Menu principal: (navegação pelo índice)")
@@ -13,7 +16,75 @@ print("0 - Sair")
 print("  ")
 
 # Opção do usuário
-opcao = input("Digite a opção desejada: ")
+opcao = int(input("Digite a opção desejada: "))
+
+match opcao:
+    case 1:
+        print("  ")
+        print("-----> Você escolheu a opção Estudantes <-----")
+        print("  ")
+    case 2:
+        print("  ")
+        print("-----> Você escolheu a opção Disciplinas <-----")
+        print("  ")
+    case 3:
+        print("  ")
+        print("-----> Você escolheu a opção Professores <-----")
+        print("  ")
+    case 4:
+        print("  ")
+        print("-----> Você escolheu a opção Turmas <-----")
+        print("  ")
+    case 5:
+        print("  ")
+        print("-----> Você escolheu a opção Matriculas <-----")
+        print("  ")
+    case 0:
+        print("  ")
+        print("AGORA VOCÊ ESTÁ SAINDO DO SISTEMA")
+        print("  ")
+    case _:        
+        print("  ")
+        print("Opção I-N-V-Á-L-I-D-A")
+        print("  ")
+
+# menu CRUD
+
+print("Menu cadastro (navegação pelo índice):")
+print("1 - Incluir")
+print("2 - Listar")
+print("3 - Atualizar")
+print("4 - Excluir")
+print("5 - Voltar ao menu principal")
+print("  ")
+
+opcao_menu = int(input("Digite a opção desejada: "))
+match opcao_menu:
+    case 1:
+        print("  ")
+        print("Incluir no cadastro")
+        print("  ")
+    case 2:
+        print("  ")
+        print("Listar cadastro")
+        print("  ")
+    case 3:
+        print("  ")
+        print("Atualizar cadastro")
+        print("  ")
+    case 4:
+        print("  ")
+        print("Excluir do cadastro")
+        print("  ")
+    case 5:
+        print("  ")
+        print("VOLTAR AO MENU PRINCIPAL")
+        print("  ")
+    case _:        
+        print("  ")
+        print("Opção I-N-V-Á-L-I-D-A")
+        print("  ")
+'''
 if opcao == "1" or opcao == "2" or opcao == "3" or opcao == "4" or opcao == "5":
     print("  ")
     print("-----> Você escolheu a opção ", opcao , "...")
@@ -46,3 +117,4 @@ else:
     print("  ")
 
     
+'''
