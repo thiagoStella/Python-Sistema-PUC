4d3edda3953364c3856c82dbd250942f1624d51c
thiagoStella <thiagostellapontes01@gmail.com>
2024-04-09 13:33:41 -0300

    inclusão navegação continua no menu

--- main.py
+++ main.py
@@ -1,120 +1,47 @@
-print("                                        ")
-print("****************************************")
-print("*****  Bem-Vindo ao Sistema PUC  *******")
-print("*****  ------------------------  *******")
-print("****************************************")
-print("                                        ")
+while True:
+    print("                                        ")
+    print("****************************************")
+    print("*****  Bem-Vindo ao Sistema PUC  *******")
+    print("*****  ------------------------  *******")
+    print("****************************************")
+    print("                                        ")
 
-# mostrar menu
-print("Menu principal: (navegação pelo índice)")
-print("1 - Estudantes")
-print("2 - Disciplinas")
-print("3 - Professores")
-print("4 - Turmas")
-print("5 - Matriculas")
-print("0 - Sair")
-print("  ")
+    # mostrar menu
+    print('MENU PRINCIPAL: ')
+    print('1 - Estudantes')
+    print('2 - Disciplinas')
+    print('3 - Professores')
+    print('4 - Turmas')
+    print('5 - Matriculas')
+    print('0 - Sair')
+    print(' ')
 
-# Opção do usuário
-opcao = int(input("Digite a opção desejada: "))
+    # Opção do usuário
+    opcao = input("Digite a opção desejada: ")
+    if opcao == '1' or opcao == '2' or opcao == '3' or opcao == '4' or opcao == '5':
+        while True:
+            print(f'===== Menu {opcao} =====\n\n')
 
-match opcao:
-    case 1:
-        print("  ")
-        print("-----> Você escolheu a opção Estudantes <-----")
-        print("  ")
-    case 2:
-        print("  ")
-        print("-----> Você escolheu a opção Disciplinas <-----")
-        print("  ")
-    case 3:
-        print("  ")
-        print("-----> Você escolheu a opção Professores <-----")
-        print("  ")
-    case 4:
-        print("  ")
-        print("-----> Você escolheu a opção Turmas <-----")
-        print("  ")
-    case 5:
-        print("  ")
-        print("-----> Você escolheu a opção Matriculas <-----")
-        print("  ")
-    case 0:
-        print("  ")
-        print("AGORA VOCÊ ESTÁ SAINDO DO SISTEMA")
-        print("  ")
-    case _:        
-        print("  ")
-        print("Opção I-N-V-Á-L-I-D-A")
-        print("  ")
+        # menu CRUD
+            print("Menu cadastro (navegação pelo índice):")
+            print("1 - Incluir")
+            print("2 - Listar")
+            print("3 - Atualizar")
+            print("4 - Excluir")
+            print("5 - Voltar ao menu principal")
+            print("  ")
+            opcao_secundaria = input("Digite a operação: ")
 
-# menu CRUD
-
-print("Menu cadastro (navegação pelo índice):")
-print("1 - Incluir")
-print("2 - Listar")
-print("3 - Atualizar")
-print("4 - Excluir")
-print("5 - Voltar ao menu principal")
-print("  ")
-
-opcao_menu = int(input("Digite a opção desejada: "))
-match opcao_menu:
-    case 1:
-        print("  ")
-        print("Incluir no cadastro")
-        print("  ")
-    case 2:
-        print("  ")
-        print("Listar cadastro")
-        print("  ")
-    case 3:
-        print("  ")
-        print("Atualizar cadastro")
-        print("  ")
-    case 4:
-        print("  ")
-        print("Excluir do cadastro")
-        print("  ")
-    case 5:
-        print("  ")
-        print("VOLTAR AO MENU PRINCIPAL")
-        print("  ")
-    case _:        
-        print("  ")
-        print("Opção I-N-V-Á-L-I-D-A")
-        print("  ")
-'''
-if opcao == "1" or opcao == "2" or opcao == "3" or opcao == "4" or opcao == "5":
-    print("  ")
-    print("-----> Você escolheu a opção ", opcao , "...")
-    print("  ")
-    # menu CRUD
-
-    print("Menu: Categoria ", opcao , " (navegação pelo índice):")
-    print("1 - Incluir")
-    print("2 - Listar")
-    print("3 - Atualizar")
-    print("4 - Excluir")
-    print("5 - Voltar ao menu principal")
-    print("  ")
-
-    opcao_menu = input("Digite a opção desejada: ")
-    if opcao_menu == "1" or opcao_menu == "2" or opcao_menu == "3" or opcao_menu == "4" or opcao_menu == "5":
-        print("  ")
-        print("-----> Você escolheu a opção ", opcao_menu )
-        print("  ")
+            if opcao_secundaria == '1' or opcao_secundaria == '2' or opcao_secundaria == '3' or opcao_secundaria == '4':
+                print(f'===== Você escolheu a operação {opcao_secundaria} =====')
+                print("  ")
+                print("  ")
+            elif opcao_secundaria =='5':
+                break
+            else:
+                print('Opção inválida')
+    elif opcao == '0':
+        print("Fazendo o logoff do sistema...\n\n")
+        break
     else:
-        print("  ")
-        print("Opção I-N-V-Á-L-I-D-A")
-        print("  ")
-elif opcao == "0":
-    print("SAINDO DO SISTEMA")
-
-else:
-    print("  ")
-    print("Opção I-N-V-Á-L-I-D-A")
-    print("  ")
-
-    
-'''
+        print("OPÇÃO INVÁLIDA!")
