1ba95c70f13b12433e640c468b7e4703ec7c5cf9
thiagoStella <thiagostellapontes01@gmail.com>
2024-04-30 11:08:14 -0300

    adicionado manipulação de arquivos JSON

--- main.py
+++ main.py
@@ -2,16 +2,13 @@
 Aluno: Thiago Stella Pontes
 Curso: Análise e Desenvolvimento de Sistemas
 '''
-estudantes = []
+import json
 
 def show_main_menu(): #função que mostra o menu principal
-    print("                                        ")
     print("****************************************")
     print("*****  Bem-Vindo ao Sistema PUC  *******")
     print("*****  ------------------------  *******")
     print("****************************************")
-    print("                                        ")
-
     print('MENU PRINCIPAL: ')
     print('1 - Estudantes')
     print('2 - Disciplinas')
@@ -22,10 +19,9 @@ def show_main_menu(): #função que mostra o menu principal
     # Opção do usuário no menu principal
     return input('Digite a opção desejada: ')
 
-def show_student_menu():
-    print('\n\n===== Menu [ESTUDANTES] =====')
+def show_sub_menu(): # função que mostra o menu de CRUD
+    print('\n===== Menu de Operações =====')
     #CRUD Estudates
-    print('Menu de cadastro:')
     print('1 - Incluir')
     print('2 - Listar')
     print('3 - Atualizar')
@@ -34,72 +30,101 @@ def show_student_menu():
     print('\n')
     return input('Escolha a operação: ')
 
-def create_student(): #adiciona os valores 'matricula', 'nome' e 'cpf' as respectivas chaves
-    aluno = {}
-    aluno['matricula'] = input('\nDigite a matrícula: ')
-    aluno['nome'] = input('Digite o nome do estudante: ')
-    aluno['cpf'] = input('Digite o CPF: ')
-    estudantes.append(aluno) #adiciona os dicionarios à lista 'estudantes'
+def create_record(nome_arquivo): #adiciona os valores 'matricula', 'nome' e 'cpf' as respectivas chaves
+    estudante = {}
+    estudante['matricula'] = input('\nDigite a matrícula: ')
+    estudante['nome'] = input('Digite o nome do estudante: ')
+    estudante['cpf'] = input('Digite o CPF: ')
+    lista = ler_arquivo('estudantes.json')
+    if lista is None:
+        lista = []
+    lista.append(estudante) #adiciona os dicionarios à lista 'estudantes'
+    salvar_arquivo(lista, nome_arquivo)
     print('=================================')
     print('Estudante cadastrado com sucesso!\n\n')
 
-def read_student(): #verifica se a lista está vazia ou imprime a lista na tela 
+def read_record(nome_arquivo): #verifica se a lista está vazia ou imprime a lista na tela 
+    estudantes = ler_arquivo(nome_arquivo)
     if estudantes == []:
         print('Não há estudantes cadastrados')
     else:
-        for aluno in estudantes:
-            for chave, valor in aluno.items():
-                print(f'{chave}: {valor}', end=' | ')
-            print()
+        for estudante in estudantes:
+            print(f'Dados do estudante: {estudante}')
 
 
 
-def update_student(): #verifica se o input consta na lista de estudantes, caso esteja abre a edição, caso não, retorna um msg.
-    editar = input('Qual é o número da matrícula para atualizar? ')
-    editado = False
-    for aluno in estudantes:
-        if aluno['matricula'] == editar:
-            editado = True            
+def update_record(codigo, nome_arquivo): #verifica se o input consta na lista de estudantes, caso esteja abre a edição, caso não, retorna um msg.
+    lista = ler_arquivo(nome_arquivo)
+    codigo = input('Qual é o número da matrícula para atualizar? ')
+    for aluno in lista:
+        if aluno['matricula'] == codigo:
             aluno['matricula'] = input("Digite a nova matrícula: ")
             aluno['nome'] = input("Digite o novo nome: ")
             aluno['cpf'] = input("Digite o novo cpf: ")
+            salvar_arquivo(lista, nome_arquivo)
             print('\nEstudante atualizado com sucesso!')
-    if not editado:
-        print(f'Matricula {editar} não encontrada')
+            return
+    print('Códuigo não encontrado.')
             
 
 
-def delete_student(): #verifica se o input consta na lista, caso esteja ele exclui o estudante, caso não, retorna uma msg.
+def delete_record(codigo, nome_arquivo): #verifica se o input consta na lista, caso esteja ele exclui o estudante, caso não, retorna uma msg.
     excluir = input('Qual é o número da matrícula para excluir? ')
     removido = False
-    for aluno in estudantes:
+    lista = ler_arquivo(nome_arquivo)
+    for aluno in lista:
         if aluno['matricula'] == excluir:
             removido = True
-            estudantes.remove(aluno)
+            lista.remove(aluno)
+            salvar_arquivo(lista, 'estudantes.json')
             print('\nEstudante excluído com sucesso!')
     if not removido:
         print(f'Código {excluir} não encontrado')        
+
+def salvar_arquivo(lista, nome_arquivo):
+    with open(nome_arquivo, 'w', encoding='utf-8') as arquivo:
+        json.dump(lista, arquivo)
+
+def ler_arquivo(nome_arquivo):
+    try:
+        with open(nome_arquivo, 'r', encoding='utf-8') as arquivo:
+            lista = json.load(arquivo)
+        return lista
+    except:
+        return []
+
+def processar_menu_secundario(opcao_secundaria, nome_arquivo):
+    if opcao_secundaria == '1': 
+        create_record(nome_arquivo)
+    elif opcao_secundaria =='2': 
+        read_record(nome_arquivo)
+    elif opcao_secundaria =='3':
+        update_record(opcao_secundaria, nome_arquivo)
+    elif opcao_secundaria =='4':
+        delete_record(opcao_secundaria, nome_arquivo)
+    elif opcao_secundaria =='5': #encerra o menu de estudantes e volta ao principal
+        return False
+    else:
+        print('Opção inválida')
+    return True
+
+arquivo_estudante = 'estudantes.json'
+arquivo_disciplina = 'disciplinas.json'
 
 while True:
     opcao = show_main_menu()
     
     if opcao == '1': #direciona para o menu de estudantes
         while True:
-            opcao_estudantes = show_student_menu()
-            if opcao_estudantes == '1': 
-                create_student()
-            elif opcao_estudantes =='2': 
-                read_student()
-            elif opcao_estudantes =='3':
-                update_student()
-            elif opcao_estudantes =='4':
-                delete_student()
-            elif opcao_estudantes =='5': #encerra o menu de estudantes e volta ao principal
+            opcao_secundaria = show_sub_menu()
+            if not processar_menu_secundario(opcao_secundaria, arquivo_estudante):
                 break
-            else:
-                print('Opção inválida')
+
     elif opcao == '2':
-        print('\n\nEM DESENVOLVIMENTO')
+        while True:
+            opcao_secundaria = show_sub_menu()
+            if not processar_menu_secundario(opcao, arquivo_disciplina):
+                break
     elif opcao == '3':
         print('\n\nEM DESENVOLVIMENTO')
     elif opcao == '4':
